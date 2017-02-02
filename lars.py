09652ac7f29825c92ae2cbe5451b1488ba4e6f97
davidyoon <davidyoon@coupang.com>
2017-02-02 09:27:41 +0900

    [REGR-170202] lars algorithm

--- lars.py
+++ lars.py
@@ -11,6 +11,8 @@
 __author__ = "Jeonghun Yoon"
 import urllib2
 import math
+import numpy as np
+import matplotlib.pyplot as plot
 from sklearn.cross_validation import train_test_split
 
 '''
@@ -69,3 +71,54 @@ stddev = math.sqrt(variance)
 # normalization
 for i, v in enumerate(y_train):
     y_train[i] = (v - mean) / stddev
+
+
+'''
+4. correlation
+5. beta updates
+'''
+# initiate beta vectors
+beta = [0.0] * n_col
+beta_matrix = []
+beta_matrix.append(beta)
+
+# TODO: How to get number of step, step size?
+n_step = 350
+step_size = 0.004
+
+# use numpy matrix
+x_train_mat = np.matrix(x_train)
+
+# residual
+for stpe in range(n_step):
+    residuals = []
+    for i, v in enumerate(x_train):
+        y_hat = np.dot(np.array(v), np.array(beta))
+        residuals.append(y_train[i] - y_hat)
+    corrs = []
+    for j in range(n_col):
+        # TODO why? correlation? how to derive?
+        corr = np.dot(x_train_mat[:, j].T, np.matrix(residuals).T) / n_row
+        corrs.append(float(corr))
+    # find maximum of abs(residual)
+    i_max = 0
+    res_ab_max = 0
+    for i, v in enumerate(corrs):
+        if abs(res_ab_max) < abs(v):
+            i_max = i
+            res_ab_max = v
+    # TODO: How to gaurante the size of step_size?
+    beta[i_max] += step_size * res_ab_max / abs(res_ab_max)
+    beta_matrix.append(beta[:])
+
+'''
+visualization
+'''
+for i in range(n_col):
+    coef_curve = [beta_matrix[k][i] for k in range(n_step)]
+    x_axis = range(n_step)
+    plot.plot(x_axis, coef_curve)
+
+plot.xlabel("Step Taken")
+plot.ylabel("Coefficient Values")
+plot.show()
